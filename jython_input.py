be2f834155b6099a6a8eb42eaab670b304e52f82
JINHAE <141704597+jython77@users.noreply.github.com>
2023-08-08 15:44:34 +0900

    Create jython_input.py

---
+++ jython_input.py
@@ -0,0 +1,16 @@
+y = int(input('What year were you born? '))
+
+gen = None
+
+if y <= 1924:
+    gen = 'the Greatest Generation'
+elif y <= 1945:
+    gen = 'the Silent Generation'
+elif y <= 1964:
+    gen = 'a baby boomer'
+elif y <= 1980:
+    gen = 'a Gen X'
+elif y <= 1996:
+    gen = 'a millennial'
+else:
+    gen = 'a Gen Z'
